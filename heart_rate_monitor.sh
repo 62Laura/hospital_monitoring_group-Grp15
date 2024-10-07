c4c63c16eb57293af9642d92c7f0660b5e250ba2
root <s.homere@alustudent.com>
2024-10-07 04:44:21 -0700

    initalisation of heart rates

---
+++ heart_rate_monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Prompt the user to enter the device name
+read -p "Enter the device name: " Monitor_name
+
+# Log file where heart rate data will be stored
+log_file="heart_rate_log.txt"
+
+# Function to generate a random heart rate between 40 and 100
+generate_heart_rate() {
+    echo $((HEART_RATE % 61 + 40))
+}
