13e24ba262f769fe4e2c6fd01bd2916c4636ee97
Manzi453 <i.manziyamu@alustudnet.com>
2024-10-11 01:41:01 -0700

    manzi.sh

---
+++ manzi.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Function to generate a random heart rate between 40 and 100
+generate_heart_rate() {
+  echo $((RANDOM % 61 + 40))
+}
+
+# Get the device name passed as an argument
+device_name=$1
+
+# Start logging heart rate data to heart_rate_log.txt
+while true
+do
+  # Get the current timestamp
+  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+  # Generate a random heart rate
+  heart_rate=$(generate_heart_rate)
+
+  # Log the data: timestamp, device name, and heart rate
+  echo "$timestamp $device_name $heart_rate" >> heart_rate_log.txt
+
+  # Wait for 1 second before the next log
+  sleep 1
+done
+
