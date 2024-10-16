ad37e63ec21e4a57ce2e9cdf8b7fd7574ae8cf58
shomere <s.homere@alustudent.com>
2024-10-16 11:52:36 -0700

    singizwa.sh

--- singizwa.sh
+++ singizwa.sh
@@ -6,6 +6,4 @@ read -p "Enter device name: " devName
 # Call the manzi.sh script with the device name as an argument
 ./manzi.sh "$devName" &
 
-# Print the background process ID
-echo "Logging process started with PID: $!"
 
