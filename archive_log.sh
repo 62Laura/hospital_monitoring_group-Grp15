bc9281108e1f655bc4fbba3e209c81397c7beb23
sonia250 <s.uwase3@alustudent.com>
2024-10-18 02:22:46 -0700

    none

--- archive_log.sh
+++ archive_log.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 LOG_FILE="heart_rate_log.txt"
+if [ -f "$LOG_FILE" ]; then
+	TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+	ARCHIVED_LOG_FILE= "$(LOG_FILE)_$(TIMESTAMP)"
+	mv "$LOG_FILE" "$ARCHIVED_LOG_FILE"
+	echo "Log file archived as: $ARCHIVED_LOG_FILE"
+else
+	echo "Log file $LOG_FILE not found!"
+fi
